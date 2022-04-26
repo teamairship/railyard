493faaae86e067f63a74d91dfe117e28a604d28f
Vincent Lopez <vincent@teamairship.com>
2022-04-26 16:29:17 -0500

    formatting

--- rails/blueprints/graphql/template.rb
+++ rails/blueprints/graphql/template.rb
@@ -1,10 +1,10 @@
 def apply_self!
-    if yes?("Would you like to install GraphQL?", :blue)
-        gem "graphql"
-        gem 'graphiql-rails', git: 'https://github.com/rmosolgo/graphiql-rails', branch: 'master'
-        run "bundle install"
-        generate "graphql:install"
-    end
+  if yes?("Would you like to install GraphQL?", :blue)
+    gem "graphql"
+    gem 'graphiql-rails', git: 'https://github.com/rmosolgo/graphiql-rails', branch: 'master'
+    run "bundle install"
+    generate "graphql:install"
+  end
 end
 
 apply_self!
