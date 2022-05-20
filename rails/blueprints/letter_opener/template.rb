a6eb609d5a2dadd20f09736b9cf5ae3dd2c4d20b
Vincent Lopez <vincent@teamairship.com>
2022-05-20 14:16:42 -0500

    finish letter opener

--- rails/blueprints/letter_opener/template.rb
+++ rails/blueprints/letter_opener/template.rb
@@ -4,6 +4,8 @@ def apply_self!
         gem "letter_opener"
       end
       run "bundle install"
+      insert_into_file 'config/environments/development.rb', "\n\tconfig.action_mailer.perform_deliveries = true", after: "config.action_mailer.delivery_method = :letter_opener"
+
     end
   end
   
